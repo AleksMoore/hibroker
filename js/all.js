c0e1c29d5c0e78e898123bc23b24bad0d15b2821
Alex Moore <34859574+AleksMoore@users.noreply.github.com>
2021-03-24 16:12:38 +0300

    Скрип для скролла к якорям

--- js/all.js
+++ js/all.js
@@ -8,4 +8,13 @@
       });
       
     });
-    })(jQuery);
+})(jQuery);
+
+
+    $(function(){
+      $("a[href^='#']").click(function(){
+              var _href = $(this).attr("href");
+              $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+              return false;
+      });
+});
