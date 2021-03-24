54bdcea443d65c546521aefc3f218aca5f3f0663
Alex Moore <34859574+AleksMoore@users.noreply.github.com>
2021-03-24 16:17:04 +0300

    Скрип для скролла к якорям

--- js/all.js
+++ js/all.js
@@ -1,11 +1,20 @@
 (function($) {
-    $(function() {
-      
-      $('ul.form__caption').on('click', 'li:not(.active)', function() {
-        $(this)
-          .addClass('active').siblings().removeClass('active')
-          .closest('div.form__tabs').find('div.form__content').removeClass('active').eq($(this).index()).addClass('active');
-      });
-      
+  $(function() {
+    
+    $('ul.form__caption').on('click', 'li:not(.active)', function() {
+      $(this)
+        .addClass('active').siblings().removeClass('active')
+        .closest('div.form__tabs').find('div.form__content').removeClass('active').eq($(this).index()).addClass('active');
     });
-    })(jQuery);
+    
+  });
+})(jQuery);
+
+
+  $(function(){
+    $("a[href^='#']").click(function(){
+            var _href = $(this).attr("href");
+            $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+            return false;
+    });
+});
